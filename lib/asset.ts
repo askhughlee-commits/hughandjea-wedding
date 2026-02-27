d17c2ea4bdabb4c95bc91ffe868a8b15f5d5613d
askhughlee-commits <askhughlee@gmail.com>
2026-02-27 23:10:54 +0900

    Update asset.ts

--- lib/asset.ts
+++ lib/asset.ts
@@ -1,4 +1,5 @@
-export const withBasePath = (path: string) => {
-    const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+export function withBasePath(path: string) {
+    const isProd = process.env.NODE_ENV === "production";
+    const basePath = isProd ? (process.env.NEXT_PUBLIC_BASE_PATH ?? "") : "";
     return `${basePath}${path}`;
-};
+}
