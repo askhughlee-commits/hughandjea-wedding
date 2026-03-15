a9b1b1f18079ccc9a52fc6635f9cdb1bd16dec8b
askhughlee-commits <askhughlee@gmail.com>
2026-03-15 23:07:54 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,18 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""; // 없으면 로컬은 ""로 빌드됨
+// const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""; // 일단 보관
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
 
-  ...(basePath
-    ? {
-      basePath,
-      assetPrefix: `${basePath}/`,
-    }
-    : {}),
+  // 커스텀 도메인(hughandjea.cloud) 붙일 거라서 일단 basePath 관련은 끔
+  // ...(basePath
+  //   ? {
+  //       basePath,
+  //       assetPrefix: `${basePath}/`,
+  //     }
+  //   : {}),
 
   images: {
     unoptimized: true,
